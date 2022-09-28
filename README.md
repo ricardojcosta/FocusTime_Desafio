2dcfa007ca070980851993b6575000a9c1e4783a
RICARDO JOSE COSTA <ricardo.j.costa@edu.sc.senai.br>
2022-09-27 22:14:19 -0300

    Finalizado parte 1 sem modulos

---
+++ README.md
@@ -0,0 +1,24 @@
+# Stage 5 - Focus Timer Desafio
+
+>Curso Explorer
+
+
+Projeto construído no curso Explorer da Rocketseat. Trata-se de um cronometro com estilo pomodoro, com botões para escolher uma música.
+
+![preview](./.github/preview.png)
+
+
+
+[ 🔗 Clique aqui para acessar](em construção)
+
+
+## ⚒ Tecnologias
+
+  - HTML 
+  - CSS 
+  - JS 
+  - Git e Github
+
+## ✉ Contato
+
+ricardoredes2006@gmail.com
--- index.js
+++ index.js
@@ -6,11 +6,91 @@ const buttonCofeeOn = document.querySelector('.card-cofeeon')
 const buttonCofeeOff = document.querySelector('.card-cofeeoff')
 const buttonFirePlaceOn = document.querySelector('.card-fireplaceon')
 const buttonFirePlaceOff = document.querySelector('.card-fireplaceoff')
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonMore = document.querySelector('.more')
+const buttonLess = document.querySelector('.less')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+const audioForest = new Audio ("/sounds/Floresta.wav")
+const audioRainon = new Audio ("/sounds/Chuva.wav")
+const audioCofee = new Audio ("/sounds/Cafeteria.wav")
+const audioFirePlace = new Audio ("/sounds/Lareira.wav")
+let minutes = Number(minutesDisplay.textContent)
+let seconds = Number(secondsDisplay.textContent)
+let timerTimeOut
+
+
+/* -----------------------Events controls ------------------------------------- */
+function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
+
+}
+
+function resetTimer() {
+    updateTimerDisplay(minutes, 0)
+    clearTimeout(timerTimeOut)
+}
+
+
+function countDown() {
+    timerTimeOut = setTimeout(function () {
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+
+        updateTimerDisplay(minutes, 0)
+
+        if (minutes <= 0) {
+            return
+        }
+
+
+        if (seconds <= 0) {
+            seconds = 3
+
+            --minutes
+        }
+
+        updateTimerDisplay(minutes, String(seconds - 1))
+
+        countDown()//recursividade
+    }, 1000)
+}
 
 
 
+buttonPlay.addEventListener('click', function () {
+    countDown()
 
-buttonForestOff.addEventListener('click' , function () {
+})
+
+buttonStop.addEventListener('click', function () {
+    resetTimer()
+})
+
+buttonMore.addEventListener('click', function () {
+    minutes = minutes + 5
+    updateTimerDisplay(minutes, seconds)
+
+})
+
+buttonLess.addEventListener('click', function () {
+    minutes = minutes - 5
+    updateTimerDisplay(minutes, seconds)
+
+})
+
+/* -----------------------Events cards ------------------------------------- */
+
+audioForest.loop = true
+audioRainon.loop = true
+audioCofee.loop = true
+audioFirePlace.loop = true
+
+
+
+buttonForestOff.addEventListener('click', function () {
     buttonForestOn.classList.remove('hide')
     buttonForestOff.classList.add('hide')
     buttonRainonOn.classList.add('hide')
@@ -19,8 +99,10 @@ buttonForestOff.addEventListener('click' , function () {
     buttonRainonOff.classList.remove('hide')
     buttonCofeeOff.classList.remove('hide')
     buttonFirePlaceOff.classList.remove('hide')
-    
-
+    audioForest.play()
+    audioRainon.pause()
+    audioCofee.pause()
+    audioFirePlace.pause()
 })
 
 buttonRainonOff.addEventListener('click', function () {
@@ -32,9 +114,13 @@ buttonRainonOff.addEventListener('click', function () {
     buttonCofeeOff.classList.remove('hide')
     buttonFirePlaceOff.classList.remove('hide')
     buttonForestOff.classList.remove('hide')
+    audioRainon.play()
+    audioCofee.pause()
+    audioForest.pause()
+    audioFirePlace.pause()
 })
 
-buttonCofeeOff.addEventListener('click', function (){
+buttonCofeeOff.addEventListener('click', function () {
     buttonCofeeOn.classList.remove('hide')
     buttonCofeeOff.classList.add('hide')
     buttonFirePlaceOn.classList.add('hide')
@@ -43,6 +129,10 @@ buttonCofeeOff.addEventListener('click', function (){
     buttonFirePlaceOff.classList.remove('hide')
     buttonForestOff.classList.remove('hide')
     buttonRainonOff.classList.remove('hide')
+    audioCofee.play()
+    audioFirePlace.pause()
+    audioForest.pause()
+    audioRainon.pause()
 })
 
 buttonFirePlaceOff.addEventListener('click', function () {
@@ -54,25 +144,32 @@ buttonFirePlaceOff.addEventListener('click', function () {
     buttonForestOff.classList.remove('hide')
     buttonRainonOff.classList.remove('hide')
     buttonCofeeOff.classList.remove('hide')
+    audioFirePlace.play()
+    audioCofee.pause()
+    audioForest.pause()
+    audioRainon.pause()
 })
 
-buttonForestOn.addEventListener('click' , function () {
+buttonForestOn.addEventListener('click', function () {
     buttonForestOn.classList.add('hide')
     buttonForestOff.classList.remove('hide')
-
+    audioForest.pause()
 })
 
 buttonRainonOn.addEventListener('click', function () {
     buttonRainonOn.classList.add('hide')
     buttonRainonOff.classList.remove('hide')
+    audioRainon.pause()
 })
 
-buttonCofeeOn.addEventListener('click', function (){
+buttonCofeeOn.addEventListener('click', function () {
     buttonCofeeOn.classList.add('hide')
     buttonCofeeOff.classList.remove('hide')
+    audioCofee.pause()
 })
 
 buttonFirePlaceOn.addEventListener('click', function () {
     buttonFirePlaceOn.classList.add('hide')
     buttonFirePlaceOff.classList.remove('hide')
+    audioFirePlace.pause()
 })
