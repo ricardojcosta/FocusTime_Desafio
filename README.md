d90f7af572ab067bd6af2c27d23958376fe7c3fb
RICARDO JOSE COSTA <ricardo.j.costa@edu.sc.senai.br>
2022-09-29 21:32:38 -0300

    Projeto Finalizado

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 >Curso Explorer
 
 
-Projeto construído no curso Explorer da Rocketseat. Trata-se de um cronometro estilo pomodoro, com botões para escolher uma música.
+Projeto construído no curso Explorer da Rocketseat. 
+Trata-se de um cronometro estilo pomodoro, com botões para escolher uma música. Temos também a opção de alternar entre o modo Light e modo Dark clicando no botão no canto superior da tela.
 
 ![preview](./.github/preview.png)
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+/* ----------------------- Cards sounds ------------------------------------- */
 const buttonForestOn = document.querySelector('.card-foreston')
 const buttonForestOff = document.querySelector('.card-forestoff')
 const buttonRainonOn = document.querySelector('.card-rainonon')
@@ -6,23 +8,39 @@ const buttonCofeeOn = document.querySelector('.card-cofeeon')
 const buttonCofeeOff = document.querySelector('.card-cofeeoff')
 const buttonFirePlaceOn = document.querySelector('.card-fireplaceon')
 const buttonFirePlaceOff = document.querySelector('.card-fireplaceoff')
+let volumeForest = document.querySelector('#sound-forest')
+let volumeRainon = document.querySelector('#sound-rainon')
+let volumeCoffee = document.querySelector('#sound-coffee')
+let volumeFirePlace = document.querySelector('#sound-fireplace')
+const audioForest = new Audio("./sounds/Floresta.wav")
+const audioRainon = new Audio("./sounds/Chuva.wav")
+const audioCofee = new Audio("./sounds/Cafeteria.wav")
+const audioFirePlace = new Audio("./sounds/Lareira.wav")
+
+/* ----------------------- Buttons controls ------------------------------------- */
 const buttonPlay = document.querySelector('.play')
 const buttonStop = document.querySelector('.stop')
 const buttonMore = document.querySelector('.more')
 const buttonLess = document.querySelector('.less')
-const buttonModeDark = document.querySelector('.mode-dark')
-const buttonModeLight = document.querySelector('.mode-light')
-const colorTimer = document.querySelector('.timer')
-const main = document.querySelector('.main')
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
-const audioForest = new Audio ("./sounds/Floresta.wav")
-const audioRainon = new Audio ("./sounds/Chuva.wav")
-const audioCofee = new Audio ("./sounds/Cafeteria.wav")
-const audioFirePlace = new Audio ("./sounds/Lareira.wav")
+const sliderForest = document.querySelector('.slider-forest')
+const sliderRainon = document.querySelector('.slider-rainon')
+const sliderCoffee = document.querySelector('.slider-cofee')
+const sliderFirePlace = document.querySelector('.slider-fireplace')
+
 let minutes = Number(minutesDisplay.textContent)
 let seconds = Number(secondsDisplay.textContent)
 let timerTimeOut
+
+/* ----------------------- Change mode ------------------------------------- */
+const buttonModeDark = document.querySelector('.mode-dark')
+const buttonModeLight = document.querySelector('.mode-light')
+const main = document.querySelector('.main')
+const colorTimer = document.querySelector('.timer')
+
+
+
 
 
 /* -----------------------Events controls ------------------------------------- */
@@ -85,14 +103,27 @@ buttonLess.addEventListener('click', function () {
 
 })
 
+/* -----------------------Events sounds ------------------------------------- */
+function toggleSound(cardMain, card2, card3, card4) {
+    if (cardMain.paused == true) {
+        cardMain.play()
+        cardMain.loop = true
+        card2.pause()
+        card3.pause()
+        card4.pause()
+    } else {
+        cardMain.pause()
+        card2.pause()
+        card3.pause()
+        card4.pause()
+    }
+}
+
+function controlVolume(soundMain, card) {
+    soundMain.volume = card.value
+}
+
 /* -----------------------Events cards ------------------------------------- */
-
-audioForest.loop = true
-audioRainon.loop = true
-audioCofee.loop = true
-audioFirePlace.loop = true
-
-
 
 buttonForestOff.addEventListener('click', function () {
     buttonForestOn.classList.remove('hide')
@@ -103,10 +134,8 @@ buttonForestOff.addEventListener('click', function () {
     buttonRainonOff.classList.remove('hide')
     buttonCofeeOff.classList.remove('hide')
     buttonFirePlaceOff.classList.remove('hide')
-    audioForest.play()
-    audioRainon.pause()
-    audioCofee.pause()
-    audioFirePlace.pause()
+    toggleSound(audioForest, audioRainon, audioCofee, audioFirePlace)
+   
 })
 
 buttonRainonOff.addEventListener('click', function () {
@@ -118,10 +147,7 @@ buttonRainonOff.addEventListener('click', function () {
     buttonCofeeOff.classList.remove('hide')
     buttonFirePlaceOff.classList.remove('hide')
     buttonForestOff.classList.remove('hide')
-    audioRainon.play()
-    audioCofee.pause()
-    audioForest.pause()
-    audioFirePlace.pause()
+    toggleSound(audioRainon, audioForest, audioCofee, audioFirePlace)
 })
 
 buttonCofeeOff.addEventListener('click', function () {
@@ -133,10 +159,7 @@ buttonCofeeOff.addEventListener('click', function () {
     buttonFirePlaceOff.classList.remove('hide')
     buttonForestOff.classList.remove('hide')
     buttonRainonOff.classList.remove('hide')
-    audioCofee.play()
-    audioFirePlace.pause()
-    audioForest.pause()
-    audioRainon.pause()
+    toggleSound(audioCofee, audioForest, audioRainon, audioFirePlace)
 })
 
 buttonFirePlaceOff.addEventListener('click', function () {
@@ -148,16 +171,17 @@ buttonFirePlaceOff.addEventListener('click', function () {
     buttonForestOff.classList.remove('hide')
     buttonRainonOff.classList.remove('hide')
     buttonCofeeOff.classList.remove('hide')
-    audioFirePlace.play()
-    audioCofee.pause()
-    audioForest.pause()
-    audioRainon.pause()
+    toggleSound(audioFirePlace, audioForest, audioRainon, audioCofee)
 })
 
 buttonForestOn.addEventListener('click', function () {
     buttonForestOn.classList.add('hide')
     buttonForestOff.classList.remove('hide')
-    audioForest.pause()
+    audioForest.pause()  
+})
+
+buttonForestOn.addEventListener('input', function () {
+    controlVolume(audioForest, volumeForest)
 })
 
 buttonRainonOn.addEventListener('click', function () {
@@ -166,10 +190,18 @@ buttonRainonOn.addEventListener('click', function () {
     audioRainon.pause()
 })
 
+buttonRainonOn.addEventListener('input', function () {
+    controlVolume(audioRainon, volumeRainon)
+})
+
 buttonCofeeOn.addEventListener('click', function () {
     buttonCofeeOn.classList.add('hide')
     buttonCofeeOff.classList.remove('hide')
     audioCofee.pause()
+})
+
+buttonCofeeOn.addEventListener('input', function () {
+    controlVolume(audioCofee, volumeCoffee)
 })
 
 buttonFirePlaceOn.addEventListener('click', function () {
@@ -178,9 +210,13 @@ buttonFirePlaceOn.addEventListener('click', function () {
     audioFirePlace.pause()
 })
 
+buttonFirePlaceOn.addEventListener('input', function () {
+    controlVolume(audioFirePlace, volumeFirePlace)
+})
+
 /* -----------------------Events mode ------------------------------------- */
 
-buttonModeLight.addEventListener('click', function(){
+buttonModeLight.addEventListener('click', function () {
     buttonModeDark.classList.remove('hide')
     buttonModeLight.classList.add('hide')
     main.classList.add('dark')
@@ -201,9 +237,17 @@ buttonModeLight.addEventListener('click', function(){
     buttonRainonOff.classList.add('card-rainonoff-d')
     buttonCofeeOff.classList.add('card-cofeeoff-d')
     buttonFirePlaceOff.classList.add('card-fireplaceoff-d')
+    sliderForest.classList.remove('slider-forest')
+    sliderForest.classList.add('slider-dark')
+    sliderRainon.classList.remove('slider-rainon')
+    sliderRainon.classList.add('slider-dark')
+    sliderCoffee.classList.remove('slider-cofee')
+    sliderCoffee.classList.add('slider-dark')
+    sliderFirePlace.classList.remove('slider-fireplace')
+    sliderFirePlace.classList.add('slider-dark')
 })
 
-buttonModeDark.addEventListener('click', function(){
+buttonModeDark.addEventListener('click', function () {
     buttonModeLight.classList.remove('hide')
     buttonModeDark.classList.add('hide')
     main.classList.remove('dark')
@@ -224,4 +268,13 @@ buttonModeDark.addEventListener('click', function(){
     buttonRainonOff.classList.remove('card-rainonoff-d')
     buttonCofeeOff.classList.remove('card-cofeeoff-d')
     buttonFirePlaceOff.classList.remove('card-fireplaceoff-d')
+    sliderForest.classList.add('slider-forest')
+    sliderForest.classList.remove('slider-dark')
+    sliderRainon.classList.add('slider-rainon')
+    sliderRainon.classList.remove('slider-dark')
+    sliderCoffee.classList.add('slider-cofee')
+    sliderCoffee.classList.remove('slider-dark')
+    sliderFirePlace.classList.add('slider-fireplace')
+    sliderFirePlace.classList.remove('slider-dark')
+
 })
